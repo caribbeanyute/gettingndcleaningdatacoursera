88daa882a0e77087539d6fb617b0244eeac1f127
caribbeanyute <smithcleon@gmail.com>
2020-10-03 17:20:55 -0500

    Completed Data cleaning project

---
+++ run_analysis.R
@@ -0,0 +1,84 @@
+# URL for dataset
+fileName <- "UCIdata.zip"
+url <- "http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+dir <- "UCI HAR Dataset"
+
+# File download verification. If file does not exist, download to working directory.
+if(!file.exists(fileName)){
+  download.file(url,fileName, mode = "wb") 
+}
+
+# File unzip verification. If the directory does not exist, unzip the downloaded file.
+if(!file.exists(dir)){
+  unzip("UCIdata.zip", files = NULL, exdir=".")
+}
+
+if (!"reshape2" %in% installed.packages()) {
+  install.packages("reshape2")
+}
+library("reshape2")
+
+
+
+
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+X_test <- read.table("UCI HAR Dataset/test/X_test.txt")
+X_train <- read.table("UCI HAR Dataset/train/X_train.txt")
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+
+activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
+features <- read.table("UCI HAR Dataset/features.txt")  
+
+## Analysis
+# 1. Merging the training and test dataset into one.
+dataSet <- rbind(X_train,X_test)
+
+# 2. Extracting the mean and standard deviations
+# Create a vector of only mean and std, use the vector to subset.
+meanandstdonly <- grep("mean()|std()", features[, 2]) 
+dataSet <- dataSet[,meanandstdonly]
+
+
+# 4. Appropriately labeleling the data set with descriptive activity names.
+# Create vector of "Clean" feature names by getting rid of "()" apply to the dataSet to rename labels.
+CleanFeatureNames <- sapply(features[, 2], function(x) {gsub("[()]", "",x)})
+names(dataSet) <- CleanFeatureNames[meanandstdonly]
+
+# Combining test and train of subject data and activity data, and adding descriptive lables.
+subject <- rbind(subject_train, subject_test)
+names(subject) <- 'subject'
+activity <- rbind(y_train, y_test)
+names(activity) <- 'activity'
+
+# combining subject, activity, and mean and std only data set to create final data set.
+dataSet <- cbind(subject,activity, dataSet)
+
+
+# 3. Uses descriptive activity names to name the activities in the data set
+# group the activity column of dataSet, re-name lable of levels with activity_levels, and apply it to dataSet.
+act_group <- factor(dataSet$activity)
+levels(act_group) <- activity_labels[,2]
+dataSet$activity <- act_group
+
+
+# 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+
+
+# melting data to tall skinny data and cast means.
+baseData <- melt(dataSet,(id.vars=c("subject","activity")))
+secondDataSet <- dcast(baseData, subject + activity ~ variable, mean)
+
+names(secondDataSet)[-c(1:2)] <- paste("[mean of]" , names(secondDataSet)[-c(1:2)] )
+write.table(secondDataSet, "tidy_data.txt", sep = ",")
+
+
+
+
+
+
+
+
+
+
